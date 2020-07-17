8fdf1a2b54d060348c6686daa7816fe026ef0e3b
draZer0 <ardkokkel@outlook.com>
2020-07-17 21:16:02 +0200

    Created middleware to check admin role

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('cars', 'CarsController');
+Route::resource('cars', 'CarsController')->middleware(['auth','role']);;
